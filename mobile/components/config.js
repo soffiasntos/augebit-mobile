8fbb36d36dfc3e2866a3ae0c5f095d8f7f609140
soffiasntos <159542070+soffiasntos@users.noreply.github.com>
2025-07-31 09:49:28 -0300

    bb

--- mobile/components/config.js
+++ mobile/components/config.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 //const LOCAL_IP = Platform.OS === 'android' ? '10.0.2.2' : 'localhost'; // Emulador padrão
- const LOCAL_IP = '192.168.1.111'; // Professor para testes em dispositivos reais troque o --- pelo ip do seu computador
+const LOCAL_IP = '10.136.23.130'; // Professor para testes em dispositivos reais troque o --- pelo ip do seu computador
 
 export const API_URL = `http://${LOCAL_IP}:3000`;
 
